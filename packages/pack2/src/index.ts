9ea6a3d46e621ec4344f374d7be2a2b410475fae
Ihor Vodianyi <vodyanoy_igor@hotmail.com>
2023-08-12 12:00:27 +0200

    Commit

--- packages/pack2/src/index.ts
+++ packages/pack2/src/index.ts
@@ -1,1 +1,32 @@
-export const namePackage = 'pack2';
+export enum BoundedStringValidateState {
+    valid = 'valid',
+    minlength = 'minlength',
+    maxlength = 'maxlength'
+}
+
+export class BoundedString {
+    public static readonly MAX_SIZE: number = 50;
+    public static readonly MIN_SIZE: number = 3;
+    //public static readonly Empty: ProductName = new ProductName('');
+
+    public readonly value: string;
+
+    protected constructor(value: string) {
+        this.value = value;
+    }
+
+    public equals(object: BoundedString): boolean {
+        return this.value === object.value;
+    }
+
+    public static create(value: string): [BoundedStringValidateState, BoundedString | undefined] {
+        const isValid = BoundedString.validate(value);
+        return [isValid, (isValid === BoundedStringValidateState.valid) ? new BoundedString(value) : undefined];
+    }
+
+    public static validate(value: string): BoundedStringValidateState {
+        if (value.length < this.MIN_SIZE) return BoundedStringValidateState.minlength;
+        if (value.length > this.MAX_SIZE) return BoundedStringValidateState.maxlength;
+        return BoundedStringValidateState.valid;
+    }
+}
